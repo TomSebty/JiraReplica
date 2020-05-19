d3562abb2c0913443e7bdb9430e9cc5475f2f7d8
Tom Seby <tom@localhost.localdomain>
2020-05-19 16:18:40 +0300

    Added card button that creates a new dummy card

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 var card = new Card("This is a header", "This is the body");
 
-card.CreateCard();
+//card.CreateCard();
+
+function AddNewCard(){
+    card.CreateCard();
+}
+
+var addCards = document.querySelectorAll('.card-add');
+
+addCards.forEach(function() {
+
+    this.onclick = AddNewCard;
+
+})
